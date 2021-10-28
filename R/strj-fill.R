7c99e322c99c489dab826011fca23b82e557409e
paithiov909 <am11.44@gmail.com>
2021-10-28 18:30:03 +0900

    refactor: fix functions

--- R/strj-fill.R
+++ R/strj-fill.R
@@ -9,7 +9,7 @@ strj_fill_iter_mark <- function(text) {
   text <- text %>%
     stringi::stri_replace_all_regex("(\uff0f\u2033\uff3c)", "\u3034\u3035") %>%
     stringi::stri_replace_all_regex("(\uff0f\uff3c)", "\u3033\u3035")
-  purrr::map_chr(text, function(res) {
+  furrr::future_map_chr(text, function(res) {
     if (nchar(res) > 4) {
       res <- magrittr::freduce(
         res,
@@ -22,90 +22,105 @@ strj_fill_iter_mark <- function(text) {
 
 #' @noRd
 fill_iter_mark_single <- function(text) {
-  textloop <- tokenizers::tokenize_characters(text) %>%
-    purrr::map(~ embed(., 2)[, 2:1]) %>%
-    purrr::map(~ as.data.frame(.))
-  lapply(textloop, function(df) {
-    df <- df %>%
-      dplyr::mutate(
-        V2 = dplyr::if_else(
-          stringi::stri_detect_regex(df$V2, "[\u30fd\u309d\u3003\u3005]"),
-          V1,
-          V2
+  if (stringi::stri_detect_regex(text, "[\u30fd\u309d\u3003]")) {
+    textloop <- stringi::stri_split_boundaries(text, type = "character") %>%
+      purrr::map(~ embed(., 2)[, 2:1]) %>%
+      purrr::map(~ as.data.frame(.))
+    lapply(textloop, function(df) {
+      df <- df %>%
+        dplyr::mutate(
+          V2 = dplyr::if_else(
+            stringi::stri_detect_regex(df$V2, "[\u30fd\u309d\u3003]"),
+            V1,
+            V2
+          )
         )
-      )
-    paste0(c(df[1, 1], df$V2), collapse = "")
-  })
+      paste0(c(df[1, 1], df$V2), collapse = "")
+    })
+  } else {
+    text
+  }
 }
 
 #' @noRd
 fill_iter_mark_single2 <- function(text) {
-  textloop <- tokenizers::tokenize_characters(text) %>%
-    purrr::map(~ embed(., 2)[, 2:1]) %>%
-    purrr::map(~ as.data.frame(.))
-  lapply(textloop, function(df) {
-    df <- df %>%
-      dplyr::mutate(
-        V2 = dplyr::if_else(
-          stringi::stri_detect_regex(df$V2, "[\u30fe\u309e]"),
-          paste0(V1, enc2utf8("\uff9e")),
-          V2
+  if (stringi::stri_detect_regex(text, "[\u30fe\u309e]")) {
+    textloop <- stringi::stri_split_boundaries(text, type = "character") %>%
+      purrr::map(~ embed(., 2)[, 2:1]) %>%
+      purrr::map(~ as.data.frame(.))
+    lapply(textloop, function(df) {
+      df <- df %>%
+        dplyr::mutate(
+          V2 = dplyr::if_else(
+            stringi::stri_detect_regex(df$V2, "[\u30fe\u309e]"),
+            paste0(V1, enc2utf8("\uff9e")),
+            V2
+          )
         )
-      )
-    paste0(c(df[1, 1], df$V2), collapse = "")
-  })
+      paste0(c(df[1, 1], df$V2), collapse = "")
+    })
+  } else {
+    text
+  }
 }
 
 #' @noRd
 fill_iter_mark_double <- function(text) {
-  textloop <- tokenizers::tokenize_characters(text) %>%
-    purrr::map(~ embed(., 4)[, 4:1]) %>%
-    purrr::map(~ as.data.frame(.))
-  lapply(textloop, function(df) {
-    df <- df %>%
-      dplyr::mutate(
-        V3 = dplyr::if_else(
-          stringi::stri_detect_regex(V3, "[\u3033]"),
-          V1,
-          V3
-        ),
-        V4 = dplyr::if_else(
-          stringi::stri_detect_regex(V4, "[\u3033]"),
-          V2,
-          V4
+  if (stringi::stri_detect_regex(text, "(\u3033\u3035)")) {
+    textloop <- stringi::stri_split_boundaries(text, type = "character") %>%
+      purrr::map(~ embed(., 4)[, 4:1]) %>%
+      purrr::map(~ as.data.frame(.))
+    lapply(textloop, function(df) {
+      df <- df %>%
+        dplyr::mutate(
+          V3 = dplyr::if_else(
+            stringi::stri_detect_regex(V3, "[\u3033]"),
+            V1,
+            V3
+          ),
+          V4 = dplyr::if_else(
+            stringi::stri_detect_regex(V4, "[\u3033]"),
+            V2,
+            V4
+          )
         )
-      )
-    paste0(c(df[1, 1:3], df$V4), collapse = "")
-  })
+      paste0(c(df[1, 1:3], df$V4), collapse = "")
+    })
+  } else {
+    text
+  }
 }
 
 #' @noRd
 fill_iter_mark_double2 <- function(text) {
-  textloop <- tokenizers::tokenize_characters(text) %>%
-    purrr::map(~ embed(., 4)[, 4:1]) %>%
-    purrr::map(~ as.data.frame(.))
-  lapply(textloop, function(df) {
-    df <- df %>%
-      dplyr::mutate(
-        V3 = dplyr::if_else(
-          stringi::stri_detect_regex(V3, "[\u3034]"),
-          paste0(V1, enc2utf8("\uff9e")),
-          V3
-        ),
-        V4 = dplyr::if_else(
-          stringi::stri_detect_regex(V4, "[\u3034]"),
-          paste0(V2, enc2utf8("\uff9e")),
-          V4
+  if (stringi::stri_detect_regex(text, "(\u3034\u3035)")) {
+    textloop <- stringi::stri_split_boundaries(text, type = "character") %>%
+      purrr::map(~ embed(., 4)[, 4:1]) %>%
+      purrr::map(~ as.data.frame(.))
+    lapply(textloop, function(df) {
+      df <- df %>%
+        dplyr::mutate(
+          V3 = dplyr::if_else(
+            stringi::stri_detect_regex(V3, "[\u3034]"),
+            paste0(V1, enc2utf8("\uff9e")),
+            V3
+          ),
+          V4 = dplyr::if_else(
+            stringi::stri_detect_regex(V4, "[\u3034]"),
+            paste0(V2, enc2utf8("\uff9e")),
+            V4
+          )
+        ) %>%
+        dplyr::mutate(
+          V4 = dplyr::if_else(
+            stringi::stri_detect_regex(V4, "[\u3035]"),
+            V2,
+            V4
+          )
         )
-      ) %>%
-      dplyr::mutate(
-        V4 = dplyr::if_else(
-          stringi::stri_detect_regex(V4, "[\u3035]"),
-          V2,
-          V4
-        )
-      )
-    paste0(c(df[1, 1:3], df$V4), collapse = "")
-  })
+      paste0(c(df[1, 1:3], df$V4), collapse = "")
+    })
+  } else {
+    text
+  }
 }
-
--- R/strj-rewrite.R
+++ R/strj-rewrite.R
@@ -1,25 +1,30 @@
 #' Rewrite text using rewrite.def
 #'
 #' @param text Character vector.
-#' @param def_path Character scalar; path to the rewriting definition file.
+#' @param as List.
 #' @returns Character vector
 #' @export
-strj_rewrite_as_def <- function(text, def_path = system.file("rewrite.def", package = "audubon")) {
+strj_rewrite_as_def <- function(text, as = read_rewrite_def()) {
   textloop <- stringi::stri_split_boundaries(text, type = "character")
-  rewrite <- read_rewrite_def(def_path)
+  pattern <- purrr::map_chr(as$replace, ~ purrr::pluck(., 1))
+  rep <- purrr::map_chr(as$replace, ~ purrr::pluck(., 2))
   furrr::future_map_chr(textloop, function(chr) {
-    flags <- chr %in% rewrite$ignore
+    flags <- chr %in% as$ignore
     purrr::reduce2(chr, flags, function(pre, nxt, is_ignored) {
       paste0(
         pre,
-        ifelse(is_ignored, nxt, apply_rep_all(nxt, rewrite$replace))
+        ifelse(is_ignored, nxt, apply_rep_all(nxt, pattern, rep))
       )
     }, .init = "")
   })
 }
 
-#' @noRd
-read_rewrite_def <- function(def_path) {
+#' Read rewrite.def file
+#'
+#' @param def_path Character scalar; path to the rewriting definition file.
+#' @returns List.
+#' @export
+read_rewrite_def <- function(def_path = system.file("rewrite.def", package = "audubon")) {
   cols <- def_path %>%
     readr::read_lines() %>%
     purrr::discard(~ stringi::stri_detect_fixed(., "#")) %>%
@@ -33,9 +38,7 @@ read_rewrite_def <- function(def_path) {
 }
 
 #' @noRd
-apply_rep_all <- function(text, def) {
-  pattern <- purrr::map_chr(def, ~ purrr::pluck(., 1))
-  rep <- purrr::map_chr(def, ~ purrr::pluck(., 2))
+apply_rep_all <- function(text, pattern, rep) {
   res <- stringi::stri_replace_all_fixed(text, pattern = pattern, replace = rep, vectorise_all = FALSE)
   return(stringi::stri_trans_nfkc(res))
 }
