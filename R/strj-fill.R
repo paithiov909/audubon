25c88360b25e1d1699c253c6776ff1cde0ad5159
paithiov909 <am11.44@gmail.com>
2021-10-28 16:07:17 +0900

    feat: replace double iteration mark

--- R/strj-fill.R
+++ R/strj-fill.R
@@ -6,13 +6,18 @@
 #' @returns Character vector.
 #' @export
 strj_fill_iter_mark <- function(text) {
-  v <- sapply(text, function(res) {
-    if (nchar(res) >= 4) {
-      res <- magrittr::freduce(res, list(fill_iter_mark_single2, fill_iter_mark_single))
+  text <- text %>%
+    stringi::stri_replace_all_regex("(\uff0f\u2033\uff3c)", "\u3034\u3035") %>%
+    stringi::stri_replace_all_regex("(\uff0f\uff3c)", "\u3033\u3035")
+  purrr::map_chr(text, function(res) {
+    if (nchar(res) > 4) {
+      res <- magrittr::freduce(
+        res,
+        list(fill_iter_mark_double2, fill_iter_mark_double, fill_iter_mark_single2, fill_iter_mark_single)
+      )
     }
-    return(res)
-  }, USE.NAMES = FALSE)
-  return(purrr::flatten_chr(v))
+    return(unlist(res))
+  })
 }
 
 #' @noRd
@@ -21,16 +26,15 @@ fill_iter_mark_single <- function(text) {
     purrr::map(~ embed(., 2)[, 2:1]) %>%
     purrr::map(~ as.data.frame(.))
   lapply(textloop, function(df) {
-    df$flags <- stringi::stri_detect_regex(df$V2, "[\u30fd\u309d\u3003\u3005]")
     df <- df %>%
       dplyr::mutate(
-        V3 = dplyr::if_else(
-          flags,
+        V2 = dplyr::if_else(
+          stringi::stri_detect_regex(df$V2, "[\u30fd\u309d\u3003\u3005]"),
           V1,
           V2
         )
       )
-    paste0(c(df[1, 1], df$V3), collapse = "")
+    paste0(c(df[1, 1], df$V2), collapse = "")
   })
 }
 
@@ -40,15 +44,68 @@ fill_iter_mark_single2 <- function(text) {
     purrr::map(~ embed(., 2)[, 2:1]) %>%
     purrr::map(~ as.data.frame(.))
   lapply(textloop, function(df) {
-    df$flags <- stringi::stri_detect_regex(df$V2, "[\u30fe\u309e]")
+    df <- df %>%
+      dplyr::mutate(
+        V2 = dplyr::if_else(
+          stringi::stri_detect_regex(df$V2, "[\u30fe\u309e]"),
+          paste0(V1, enc2utf8("\uff9e")),
+          V2
+        )
+      )
+    paste0(c(df[1, 1], df$V2), collapse = "")
+  })
+}
+
+#' @noRd
+fill_iter_mark_double <- function(text) {
+  textloop <- tokenizers::tokenize_characters(text) %>%
+    purrr::map(~ embed(., 4)[, 4:1]) %>%
+    purrr::map(~ as.data.frame(.))
+  lapply(textloop, function(df) {
     df <- df %>%
       dplyr::mutate(
         V3 = dplyr::if_else(
-          .data$flags,
-          paste0(V1, enc2utf8("\uff9e")),
-          paste0(V2)
+          stringi::stri_detect_regex(V3, "[\u3033]"),
+          V1,
+          V3
+        ),
+        V4 = dplyr::if_else(
+          stringi::stri_detect_regex(V4, "[\u3033]"),
+          V2,
+          V4
         )
       )
-    paste0(c(df[1, 1], df$V3), collapse = "")
+    paste0(c(df[1, 1:3], df$V4), collapse = "")
   })
 }
+
+#' @noRd
+fill_iter_mark_double2 <- function(text) {
+  textloop <- tokenizers::tokenize_characters(text) %>%
+    purrr::map(~ embed(., 4)[, 4:1]) %>%
+    purrr::map(~ as.data.frame(.))
+  lapply(textloop, function(df) {
+    df <- df %>%
+      dplyr::mutate(
+        V3 = dplyr::if_else(
+          stringi::stri_detect_regex(V3, "[\u3034]"),
+          paste0(V1, enc2utf8("\uff9e")),
+          V3
+        ),
+        V4 = dplyr::if_else(
+          stringi::stri_detect_regex(V4, "[\u3034]"),
+          paste0(V2, enc2utf8("\uff9e")),
+          V4
+        )
+      ) %>%
+      dplyr::mutate(
+        V4 = dplyr::if_else(
+          stringi::stri_detect_regex(V4, "[\u3035]"),
+          V2,
+          V4
+        )
+      )
+    paste0(c(df[1, 1:3], df$V4), collapse = "")
+  })
+}
+
