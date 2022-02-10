196ee3c30aa14934fdec3621b2ada6c9cc958ae5
paithiov909 <am11.44@gmail.com>
2022-02-10 13:43:08 +0900

    chore: add workflow

--- R/utils-pipe.R
+++ R/utils-pipe.R
@@ -6,7 +6,7 @@
 #' @rdname pipe
 #' @keywords internal
 #' @export
-#' @import dplyr
+#' @importFrom magrittr %>%
 #' @usage lhs \%>\% rhs
 #' @param lhs A value or the magrittr placeholder.
 #' @param rhs A function call using the magrittr semantics.
