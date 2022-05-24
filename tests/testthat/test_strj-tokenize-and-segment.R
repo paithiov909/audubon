a0344c957ec5e5e355c41ad6a602134ebbd69f8f
paithiov909 <am11.44@gmail.com>
2022-05-24 19:33:03 +0900

    test: add tests for 'format=data.frame'

--- tests/testthat/test_strj-tokenize-and-segment.R
+++ tests/testthat/test_strj-tokenize-and-segment.R
@@ -9,15 +9,21 @@ str <- paste0(
 
 test_that("tokenize works", {
   li <- unname(strj_tokenize(str))
+  df <- strj_tokenize(str, format = "data.frame")
   expect_equal(li[[1]][1], enc2utf8("\u3042\u306e"))
+  expect_equal(df[1, 2], enc2utf8("\u3042\u306e"))
 })
 
 test_that("segment works", {
   li <- unname(strj_segment(str))
+  df <- strj_segment(str, format = "data.frame")
   expect_equal(li[[1]][4], enc2utf8("\u98a8"))
+  expect_equal(df[4, 2], enc2utf8("\u98a8"))
 })
 
 test_that("tinyseg works", {
   li <- unname(strj_tinyseg(str))
+  df <- strj_tinyseg(str, format = "data.frame")
   expect_equal(li[[1]][3], enc2utf8("\u30a4\u30fc\u30cf\u30c8\u30fc\u30f4\u30a9"))
+  expect_equal(df[3, 2], enc2utf8("\u30a4\u30fc\u30cf\u30c8\u30fc\u30f4\u30a9"))
 })
