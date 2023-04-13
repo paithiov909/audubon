d8435da8198aa3ffa72b9c7ef93bc13c5229c1a0
Hadley Wickham <hadley@posit.co>
2023-04-13 09:45:37 -0500

    Fix test problem exposed by dev waldo (#19)
    (And fix a partial matching warning that I noticed)

--- tests/testthat/test-strj-rewrite-and-normalize.R
+++ tests/testthat/test-strj-rewrite-and-normalize.R
@@ -3,7 +3,7 @@ test_that("strj_rewrite works", {
   expect_equal(
     strj_rewrite_as_def(c(NA_character_, "\u2160\u2161\u2162", "123")),
     c(
-      "NA", ## this is expected behavior.
+      NA, ## this is expected behavior.
       "\u2160\u2161\u2162",
       "123"
     )
