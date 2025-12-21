a2a52b434e7eece6e0bd4f70a4cc707a791e0bcb
paithiov909 <am11.44@gmail.com>
2025-12-21 17:25:29 +0900

    test: skip on ci

--- tests/testthat/test-labellers.R
+++ tests/testthat/test-labellers.R
@@ -15,6 +15,10 @@ test_that("label_wrap_jp works", {
 })
 
 test_that("label_date_jp works", {
+  # This test seems to be locale-sensitive.
+  skip_on_cran()
+  skip_on_ci()
+
   date_range <- function(start, days) {
     start <- as.POSIXct(start)
     c(start, start + days * 24 * 60 * 60)
