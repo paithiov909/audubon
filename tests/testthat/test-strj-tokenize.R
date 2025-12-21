b3e0c52308978fee0cd65758e70427d3e79b9952
paithiov909 <am11.44@gmail.com>
2025-12-21 15:11:36 +0900

    tweak tests

--- tests/testthat/test-strj-tokenize.R
+++ tests/testthat/test-strj-tokenize.R
@@ -1,36 +1,26 @@
-str <- paste0(
-  c(
-    "\u3042\u306e\u30a4\u30fc\u30cf\u30c8",
-    "\u30fc\u30f4\u30a9\u306e\u3059\u304d",
-    "\u3068\u304a\u3063\u305f\u98a8"
-  ),
-  collapse = ""
-)
-
 ### strj_segment ----
 test_that("segment works", {
-  li <- unname(strj_segment(str))
-  df <- strj_segment(str, format = "data.frame")
-  expect_equal(li[[1]][5], enc2utf8("\u98a8"))
-  expect_equal(df[5, 2], enc2utf8("\u98a8"))
+  li <- unname(strj_segment(polano[4:8]))
+  expect_snapshot_value(li, style = "json2")
+  df <- strj_segment(polano[4:8], format = "data.frame")
+  expect_equal(nrow(df), sum(lengths(li)))
+  expect_equal(nlevels(df[["doc_id"]]), length(polano[4:8]))
 })
 
 ### strj_tinyseg ----
 test_that("tinyseg works", {
-  skip("tinyseg is not available yet.")
-  li <- unname(strj_tinyseg(str))
-  df <- strj_tinyseg(str, format = "data.frame")
-  expect_equal(
-    li[[1]][3],
-    enc2utf8("\u30a4\u30fc\u30cf\u30c8\u30fc\u30f4\u30a9")
-  )
-  expect_equal(df[3, 2], enc2utf8("\u30a4\u30fc\u30cf\u30c8\u30fc\u30f4\u30a9"))
+  li <- unname(strj_tinyseg(polano[4:8]))
+  expect_snapshot_value(li, style = "json2")
+  df <- strj_tinyseg(polano[4:8], format = "data.frame")
+  expect_equal(nrow(df), sum(lengths(li)))
+  expect_equal(nlevels(df[["doc_id"]]), length(polano[4:8]))
 })
 
 ### strj_tokenize ----
 test_that("tokenize works", {
-  li <- unname(strj_tokenize(str))
-  df <- strj_tokenize(str, format = "data.frame")
-  expect_equal(li[[1]][1], enc2utf8("\u3042\u306e"))
-  expect_equal(df[1, 2], enc2utf8("\u3042\u306e"))
+  li <- unname(strj_tokenize(polano[4:8]))
+  expect_snapshot_value(li, style = "json2")
+  df <- strj_tokenize(polano[4:8], format = "data.frame")
+  expect_equal(nrow(df), sum(lengths(li)))
+  expect_equal(nlevels(df[["doc_id"]]), length(polano[4:8]))
 })
